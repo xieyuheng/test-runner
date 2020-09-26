96c537117d91d6f62d721c90251d40a66f70ec3e
Xie Yuheng <xyheme@gmail.com>
2020-09-26 14:16:54 +0800

    up

--- src/matrix-expand.js
+++ src/matrix-expand.js
@@ -2,13 +2,15 @@ const files_unfold = require("./files-unfold")
 
 async function matrix_expand(matrix) {
   let array = []
-  for (const [key, value] of Object.entries(matrix)) {
-    const files = await files_unfold(value)
-    array = files.flatMap((file) =>
-      array.length === 0
-        ? [{ [key]: file }]
+  if (matrix) {
+    for (const [key, value] of Object.entries(matrix)) {
+      const files = await files_unfold(value)
+      array = files.flatMap((file) =>
+        array.length === 0
+          ? [{ [key]: file }]
         : array.map((obj) => ({ ...obj, [key]: file }))
-    )
+      )
+    }
   }
   return array
 }
