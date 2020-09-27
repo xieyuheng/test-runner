a9e908acaab17b8514c4a577da99cb3a3f740753
Xie Yuheng <xyheme@gmail.com>
2020-09-27 11:07:47 +0800

    up

--- src/expect.js
+++ src/expect.js
@@ -5,7 +5,6 @@ const ok = ({ command, stdout, stderr, elapse, error }) => {
   const header = chalk.bold.blue("[expect.ok]")
   line_report(header, { elapse, command })
   if (stdout) console.log(stdout)
-  if (stderr) console.error(stderr)
   if (error) throw error
 }
 
