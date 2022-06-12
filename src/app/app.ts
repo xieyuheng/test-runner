c98434175aab967b8dfdfed6aa2c7a2d7dbfa443
Xie Yuheng <xyheme@gmail.com>
2022-06-12 17:45:47 +0800

    up

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,7 @@
-import { customAlphabet } from "nanoid"
 import * as Loggers from "../loggers"
 import { AppConfig } from "./app-config"
 
 export class App {
-  nanoid = customAlphabet("1234567890abcdef", 16)
   config = new AppConfig()
   logger = new Loggers.PrettyLogger()
 }
